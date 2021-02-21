84808ae8c31600298a99a37101245e00e7b2c48f
Estevan Sinarta <estevan_sinarta@yahoo.ca>
2021-02-20 16:43:08 -0800

    added global font and removed border around button

--- src/AppButton.js
+++ src/AppButton.js
@@ -1,10 +1,15 @@
+
+
 const AppButton = ({ text, onClick }) => {
   const style = {
     color: "black",
     backgroundColor: "#26D863",
     padding: "10px",
-    fontFamily: "Arial",
-    borderRadius: "50px"
+    outline: 0,
+    borderRadius: "50px",
+    border: "none",
+    cursor: "pointer",
+    margin: 20,
   };
   return (
     <button style={style} onclick={onClick}>
