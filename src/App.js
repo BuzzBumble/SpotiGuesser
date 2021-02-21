f18c10ca7650848c6414298d90b6bb1780398c0f
Vivian Wu <wu.vivian00@gmail.com>
2021-02-21 00:36:39 -0800

    changed country trackers to lists; answer check doesn't work

--- src/App.js
+++ src/App.js
@@ -7,20 +7,30 @@ import AppButton from "./AppButton";
 import {useEffect, useState} from 'react';
 
 export default function App(){
-  const [trackCountry, setTrackCountry] = useState("");
-  const [selectedCountry, setSelectedCountry] = useState("");
   const [score, setScore] = useState(0);
   const [choice, setChoice] = useState({});
-  const handleChoice = country => {
-    setChoice(country);
-  };
-  
+  const trackCountryHistory = [];
+  const selectedCountryHistory = [];
+  // const [trackCountryHistory, setTrackCountry] = useState([]);
+  // const [selectedCountryHistory, setSelectedCountry] = useState([]);
+  function calcTurn(){
+    selectedCountryHistory.push(choice.ISO_A2.toLowerCase());
+    
+    if(selectedCountryHistory[selectedCountryHistory.length-1] === trackCountryHistory[trackCountryHistory.length-1]){
+      console.log("correct");
+      setScore(score+1);
+    }
+  }
+
   return (
     <div className="App">
       <h1>SpotiGuessr</h1>
-      <MapComponent onCountryChange={handleChoice} />
-      <Player url={currentTrack.preview_url} />
-      <AppButton text="Make Guess" />
+      <MapComponent onCountryChange={(country) => {setChoice(country);}} />
+      <Player setCountry={(trackCountry)=>{
+        console.log(`player:${trackCountry}`)
+        trackCountryHistory.push(trackCountry);
+        console.log(trackCountryHistory)}}/>
+      <AppButton text="Make Guess" onClick={calcTurn}/>
       <div>Choice: {choice.ADMIN}</div>
     </div>
   );
